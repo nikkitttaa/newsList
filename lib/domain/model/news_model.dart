ac41e3e787083e804086590e672306b7437222ff
nikitttaa <115147883+nikkitttaa@users.noreply.github.com>
2024-04-15 14:51:16 +0300

    #49431 | add freezed

--- lib/domain/model/news_model.dart
+++ lib/domain/model/news_model.dart
@@ -1,15 +1,20 @@
-class News {
-  final int id;
-  final String title;
-  final String imageUrl;
-  final String summary;
-  final String siteUrl;
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  News({
-    required this.id,
-    required this.title,
-    required this.imageUrl,
-    required this.summary,
-    required this.siteUrl,
-  });
+part 'news_model.freezed.dart';
+
+@freezed
+class News with _$News{
+  // final int id;
+  // final String title;
+  // final String imageUrl;
+  // final String summary;
+  // final String siteUrl;
+
+  const factory News({
+    required int id,
+    required String title,
+    required String imageUrl,
+    required String summary,
+    required String siteUrl,
+  }) = _News;
 }
